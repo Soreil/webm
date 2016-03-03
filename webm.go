f30187bb296e7b5b7e19448cdf1aa0b2428c9565
Sjon Kosse <sjon@x61.eu>
2016-03-03 19:06:15 +0100

    Fixed warnings in the C code

--- webm.go
+++ webm.go
@@ -1,7 +1,6 @@
 package webm
 
 // #cgo pkg-config: libavcodec libavutil libavformat libswscale
-// #cgo CFLAGS: -Wno-int-conversion -Wno-incompatible-pointer-types
 /*
 
 #include <libavcodec/avcodec.h>
@@ -11,11 +10,7 @@ package webm
 #include <stdio.h>
 
 #define PIX_FMT_CHOSEN PIX_FMT_RGBA
-//Buffer Size usage is a bit bugged out, make it big!
 #define BUFFER_SIZE 4096
-
-#define CHECK_ERR(ERR) {if ((ERR)<0) return ERR; }
-
 
 struct buffer_data {
     uint8_t *ptr;
@@ -38,40 +33,39 @@ AVFrame * extract_webm_image(unsigned char *opaque,size_t len)
 	av_register_all();
 	avcodec_register_all();
 
-	//TODO(sjon): Buffer memory has to be allocated by av_malloc()
-	//free with av_free()
 	unsigned char *buffer = (unsigned char*)av_malloc(BUFFER_SIZE+FF_INPUT_BUFFER_PADDING_SIZE);
 
-	//NEW BUFFER THING
 	struct buffer_data bd = {0};
 	bd.ptr = opaque;
 	bd.size = len;
 
 	//Allocate avioContext
-	//Has to be av_free()'d
-	//TODO(sjon): Implement custom reader
 	AVIOContext *ioCtx = avio_alloc_context(buffer,BUFFER_SIZE,0,&bd,&read_packet,NULL,NULL);
 
-	//destroy with avformat_free_contex()
 	AVFormatContext * ctx = avformat_alloc_context();
 
 	//Set up context to read from memory
 	ctx->pb = ioCtx;
 
-	//TODO(sjon):Actually implement the not file based option
 	//open takes a fake filename when the context pb field is set up
 	int err = avformat_open_input(&ctx, "dummyFileName", NULL, NULL);
-	CHECK_ERR(err);
+	if (err < 0) {
+		return NULL;
+	}
 
 	err = avformat_find_stream_info(ctx,NULL);
-	CHECK_ERR(err);
+	if (err < 0) {
+		return NULL;
+	}
 
 	AVCodec * codec = NULL;
 	int strm = av_find_best_stream(ctx, AVMEDIA_TYPE_VIDEO, -1, -1, &codec, 0);
 
 	AVCodecContext * codecCtx = ctx->streams[strm]->codec;
 	err = avcodec_open2(codecCtx, codec, NULL);
-	CHECK_ERR(err);
+	if (err < 0) {
+		return NULL;
+	}
 
 	struct SwsContext * swCtx = sws_getContext(codecCtx->width,
 			codecCtx->height,
@@ -85,21 +79,25 @@ AVFrame * extract_webm_image(unsigned char *opaque,size_t len)
 	{
 		AVPacket pkt;
 		err = av_read_frame(ctx, &pkt);
-		CHECK_ERR(err);
+		if (err < 0) {
+			return NULL;
+		}
 
 		if (pkt.stream_index == strm)
 		{
 			int got = 0;
 			AVFrame * frame = av_frame_alloc();
 			err = avcodec_decode_video2(codecCtx, frame, &got, &pkt);
-			CHECK_ERR(err);
+			if (err < 0) {
+				return NULL;
+			}
 
 			if (got)
 			{
 				AVFrame * rgbFrame = av_frame_alloc();
 				avpicture_alloc((AVPicture *)rgbFrame, PIX_FMT_CHOSEN, codecCtx->width, codecCtx->height);
-				sws_scale(swCtx, frame->data, frame->linesize, 0, frame->height, rgbFrame->data, rgbFrame->linesize);
-				//TODO(sjon): sws_scale performed, we can dump the pixels at this point to work with as raw RGBA data
+
+				sws_scale(swCtx,(const unsigned char * const*) frame->data, frame->linesize, 0, frame->height, rgbFrame->data, rgbFrame->linesize);
 				rgbFrame->height = frame->height;
 				rgbFrame->width = frame->width;
 				rgbFrame->format = frame->format;
@@ -109,6 +107,7 @@ AVFrame * extract_webm_image(unsigned char *opaque,size_t len)
 				av_free(buffer);
 				//avformat_free_context(ctx);
 				av_frame_free(&frame);
+
 				return rgbFrame;
 			}
 			av_frame_free(&frame);
